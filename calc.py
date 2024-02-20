34e124d73b4ea0a09f265ad8be1161017330d4c2
archspiron <78673425+archspiron@users.noreply.github.com>
2024-02-19 18:57:59 -0600

    Add files via upload

--- calc.py
+++ calc.py
@@ -1,13 +1,13 @@
 while True:
-    Bp = float(input("Enter Buy Price: "))
-    Mv = float(input("Enter Market Value: "))
-    Sp = float(input("Enter Sale Price: "))
+    BuyPrice = float(input("Enter Buy Price: "))
+    MarketValue = float(input("Enter Market Value: "))
+    SalePrice = float(input("Enter Sale Price: "))
 
-    Var0 = (Sp - Bp)
-    Var1 = ((Mv / Var0)*100)
-    Var2 = ((Bp / Var0)*100)
-    Var3 = (Var1 - Var2)
+    TotalComission = (SalePrice - BuyPrice)
+    MarketPercentage = ((MarketValue / TotalComission)*100)
+    BuyPercentage = ((BuyPrice / TotalComission)*100)
+    BuyerComission = (MarketPercentage - BuyPercentage)
 
-    print(f'Buyers comission: {Var3}')
+    print(f'Buyers comission: {BuyerComission}')
     if input('Continue? Y/N: ')in ('n', 'N'):
         break
