106a6d0e39bba2b43e8c0f06e65aadff8bb59531
archspiron <78673425+archspiron@users.noreply.github.com>
2024-02-19 18:52:12 -0600

    Update calc.py

--- calc.py
+++ calc.py
@@ -2,9 +2,10 @@ while True:
     Bp = float(input("Enter Buy Price: "))
     Mv = float(input("Enter Market Value: "))
     Sp = float(input("Enter Sale Price: "))
-    
-    Var1 = ((Mv / Sp)*100)
-    Var2 = ((Bp / Sp)*100)
+
+    Var0 = (Sp - Bp)
+    Var1 = ((Mv / Var0)*100)
+    Var2 = ((Bp / Varo)*100)
     Var3 = (Var1 - Var2)
 
     print(f'Buyers comission: {Var3}')
